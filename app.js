b2a5a27673cbb3ce7cdff07ee973e7b422e86829
busrakaraa <busraakra@gmail.com>
2021-10-06 14:12:25 +0300

    Version 3

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ document.addEventListener('DOMContentLoaded', () => {
     let score = 0
 
   
-    //The Tetrominoes
+    //The Tetris elements
     const lTetromino = [
       [1, width+1, width*2+1, 2],
       [width, width+1, width+2, width*2+2],
@@ -60,7 +60,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function draw() {
       current.forEach(index => {
         squares[currentPosition + index].classList.add('tetromino')
-        squares[currentPosition + index].style.backgroundColor = colors[random]
       })
     }
   
@@ -68,7 +67,6 @@ document.addEventListener('DOMContentLoaded', () => {
     function undraw() {
       current.forEach(index => {
         squares[currentPosition + index].classList.remove('tetromino')
-        squares[currentPosition + index].style.backgroundColor = ''
   
       })
     }
@@ -143,21 +141,21 @@ document.addEventListener('DOMContentLoaded', () => {
       return current.some(index=> (currentPosition + index) % width === 0)
     }
     
-    function checkRotatedPosition(P){
-      P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
-      if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
-        if (isAtRight()){            //use actual position to check if it's flipped over to right side
-          currentPosition += 1    //if so, add one to wrap it back around
-          checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
-          }
-      }
-      else if (P % width > 5) {
-        if (isAtLeft()){
-          currentPosition -= 1
-        checkRotatedPosition(P)
-        }
-      }
-    }
+    // function checkRotatedPosition(P){
+    //   P = P || currentPosition       //get current position.  Then, check if the piece is near the left side.
+    //   if ((P+1) % width < 4) {         //add 1 because the position index can be 1 less than where the piece is (with how they are indexed).     
+    //     if (isAtRight()){            //use actual position to check if it's flipped over to right side
+    //       currentPosition += 1    //if so, add one to wrap it back around
+    //       checkRotatedPosition(P) //check again.  Pass position from start, since long block might need to move more.
+    //       }
+    //   }
+    //   else if (P % width > 5) {
+    //     if (isAtLeft()){
+    //       currentPosition -= 1
+    //     checkRotatedPosition(P)
+    //     }
+    //   }
+    // }
     
     //rotate the tetromino
     function rotate() {
@@ -194,11 +192,9 @@ document.addEventListener('DOMContentLoaded', () => {
       //remove any trace of a tetromino form the entire grid
       displaySquares.forEach(square => {
         square.classList.remove('tetromino')
-        square.style.backgroundColor = ''
       })
       upNextTetrominoes[nextRandom].forEach( index => {
         displaySquares[displayIndex + index].classList.add('tetromino')
-        displaySquares[displayIndex + index].style.backgroundColor = colors[nextRandom]
       })
     }
   
@@ -226,7 +222,6 @@ document.addEventListener('DOMContentLoaded', () => {
           row.forEach(index => {
             squares[index].classList.remove('taken')
             squares[index].classList.remove('tetromino')
-            squares[index].style.backgroundColor = ''
           })
           const squaresRemoved = squares.splice(i, width)
           squares = squaresRemoved.concat(squares)
